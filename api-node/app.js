5e4ef0914e0d9f11881652303cafa0e7255199d9
jhreyes483 <jhreyes483@misena.edu.co>
2023-12-06 18:24:48 -0500

    creacion de arcivos iniciales de conexion a db

---
+++ api-node/app.js
@@ -0,0 +1,72 @@
+'use strict'
+
+
+// Requires express
+var express    = require('express');
+var bodyParser = require('body-parser');
+
+// Ejecutar express
+var app = express();
+// Middlewares
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+// Cargar archivos de rutas
+// var user_routes  = require('./routes/user')
+// var topic_routes = require('./routes/topic')
+// var comment_routes = require('./routes/comment')
+
+// Middlewares
+
+// CORS
+// configurar cabeceras http
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+ 
+    next();
+});
+/******************** */
+
+/*
+
+// Reescribir rutas
+app.use('/api', user_routes); //midelware
+app.use('/api',topic_routes);
+app.use('/api',comment_routes);
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*****************  pruebas ********************************* */
+//Ruta de prueba
+
+/*
+app.post('/prueba',(req, res)=>{
+    return res.status(200).send({
+        message: 'Hola mundo desde el backend node POST'
+    })
+})
+
+app.get('/prueba',(req, res)=>{
+    return res.status(200).send({
+        message: 'Hola mundo desde el backend node GTE'
+    })
+})
+
+*/
+// Exportar modulo
+module.exports = app;
---
+++ api-node/index.js
@@ -0,0 +1,22 @@
+'user scrict'
+
+var mongoose = require('mongoose');
+var app      = require('./app'); //
+var port     = process.env.PORT || 3999;
+
+
+mongoose.Promise = global.Promise;
+// mongoose.connect('mongodb://localhost:27017/api_rest_node',{
+mongoose.connect('mongodb://192.168.0.3:27017/api_rest_front',{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+    }).then(()=>{
+        console.log('La conexion a la base de datos de mongo se ha realizado');
+
+        // Crear el servidor
+        app.listen(port,() => {
+            console.log('el servidor esta corriendo perfectamente http://localhost:3999 esta funcionando');
+        })
+
+    }).catch(error => console.log(error) );
