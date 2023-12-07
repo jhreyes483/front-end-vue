e225fd8f7d24d0b06be4e4f3eac34049114062ab
jhreyes483 <jhreyes483@misena.edu.co>
2023-12-07 10:26:56 -0500

    importar css

--- proyect-vue/src/App.vue
+++ proyect-vue/src/App.vue
@@ -10,6 +10,9 @@
 <script>
 import HelloWorld from './components/HelloWorld.vue'
 import MiComponente from './components/MiComponente.vue';
+//import './assets/js/bootstrap.bundle.min.js';
+//import './assets/js/popper.min.js';
+//import './assets/js/bootstrap.min.js';
 
 export default {
   name: 'App',
@@ -17,7 +20,9 @@ export default {
     HelloWorld,
     MiComponente
   }
+  
 }
+
 </script>
 
 <style>
@@ -29,4 +34,7 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
+@import './assets/css/bootstrap.min.css';
 </style>
+
--- proyect-vue/src/components/MiComponente.vue
+++ proyect-vue/src/components/MiComponente.vue
@@ -2,8 +2,12 @@
 <template>
     <hr>
     <div id="miComponente">
-        <h1> Este es mi componente</h1>
+<div class="card card-body">
+    <h1> Este es mi componente</h1>
+</div>
         <h2>{{ subtitulo }}</h2>
+
+        <div class="btn btn-primary" >aca</div>
     </div>
     <hr>
 </template>
