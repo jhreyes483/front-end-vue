981943ebdce5ff9d02ea77b38ab2b60399347365
jhreyes483 <jhreyes483@misena.edu.co>
2023-12-07 09:44:26 -0500

    crecion de componente

--- proyect-vue/src/components/MiComponente.vue
+++ proyect-vue/src/components/MiComponente.vue
@@ -1,2 +1,18 @@
 
-<template></template>
+<template>
+    <div id="miComponente">
+        <h1> Este es mi componente</h1>
+        <h2>{{ subtitulo }}</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "MiComponente",
+    data() {
+        return {
+            subtitulo: 'Soy el subtitulo de mi componente'
+        }
+    }
+}
+</script>
