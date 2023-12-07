02b195df4f151c4a69ee9da0f9c0d1639a49bfce
jhreyes483 <jhreyes483@misena.edu.co>
2023-12-06 20:27:18 -0500

    api->fin

--- api-node/app.js
+++ api-node/app.js
@@ -1,74 +1,30 @@
 'use strict'
 
-
-// Requires express
-var express    = require('express');
+// Cargar modulos de node para crear servidor
+var express = require('express');
 var bodyParser = require('body-parser');
 
-// Ejecutar express
+// Ejecutar express (http)
 var app = express();
-// Middlewares
+
+// Cargar ficheros rutas
+var article_routes = require('./routes/article');
+
+// Middlewares 
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 
-// Cargar archivos de rutas
-// var user_routes  = require('./routes/user')
-// var topic_routes = require('./routes/topic')
-// var comment_routes = require('./routes/comment')
-var article_routes = require('./routes/article');
-
-// Middlewares
-
 // CORS
-// configurar cabeceras http
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
     res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
- 
     next();
 });
-/******************** */
 
+// Añadir prefijos a rutas / Cargar rutas
 app.use('/api', article_routes);
-/*
 
-// Reescribir rutas
-app.use('/api', user_routes); //midelware
-app.use('/api',topic_routes);
-app.use('/api',comment_routes);
-
-*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*****************  pruebas ********************************* */
-//Ruta de prueba
-
-/*
-app.post('/prueba',(req, res)=>{
-    return res.status(200).send({
-        message: 'Hola mundo desde el backend node POST'
-    })
-})
-
-app.get('/prueba',(req, res)=>{
-    return res.status(200).send({
-        message: 'Hola mundo desde el backend node GTE'
-    })
-})
-
-*/
-// Exportar modulo
+// Exportar modulo (fichero actual)
 module.exports = app;
