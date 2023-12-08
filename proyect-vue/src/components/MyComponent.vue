2a7c17542047b1c56d380d8d1c458ec27ab7abb1
jhreyes483 <jhreyes483@misena.edu.co>
2023-12-07 23:19:22 -0500

    v-if and else

--- proyect-vue/src/components/MyComponent.vue
+++ proyect-vue/src/components/MyComponent.vue
@@ -8,10 +8,28 @@
         <div class="col-md-4 mx-auto">
             <input type="text" v-model="nombre" class="form-control">
         </div>
-       <b>{{ nombre }}</b> 
-       <pre>
+        <b>{{ nombre }}</b>
+        <pre>
         {{ $data }}
        </pre>
+
+        <h2>Directivas condicionales</h2>
+        <div class="col-md-4 mx-auto">
+            <input type="number" v-model="edad" class="form-control">
+            Mi edad es: <b>{{ edad }}</b>
+        </div>
+
+        <p v-if="edad && edad >= 18 && edad < 65" style="color:red; font-weight:bold;">
+            Eres mayor de edad
+        </p>
+        <p v-else-if="edad && edad >= 65" style="color:rgb(0, 153, 255); font-weight:bold;">
+            Eres de avanzada edad
+        </p>
+
+        <p v-else style="color:rgb(0, 255, 55); font-weight:bold;">
+            Eres menor de edad
+        </p>
+
     </section>
 </template>
 
@@ -21,7 +39,9 @@ export default {
     data() {
         return {
             subtitulo: 'Soy el subtitulo de mi componente',
-            nombre: ''
+            nombre: '',
+            edad: 18
+
         }
     }
 }
