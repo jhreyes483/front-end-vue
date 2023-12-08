731dc7b5feedafab7c6c03dfee19eed0d2e1e149
jhreyes483 <jhreyes483@misena.edu.co>
2023-12-07 20:52:51 -0500

    ver toda la data y v-model

--- proyect-vue/src/components/MyComponent.vue
+++ proyect-vue/src/components/MyComponent.vue
@@ -1,6 +1,17 @@
 <template>
     <section id="content">
         <h2>Mi componente</h2>
+
+
+        <hr>
+        <h2>Reactividad</h2>
+        <div class="col-md-4 mx-auto">
+            <input type="text" v-model="nombre" class="form-control">
+        </div>
+       <b>{{ nombre }}</b> 
+       <pre>
+        {{ $data }}
+       </pre>
     </section>
 </template>
 
@@ -9,7 +20,8 @@ export default {
     name: "MiComponent",
     data() {
         return {
-            subtitulo: 'Soy el subtitulo de mi componente'
+            subtitulo: 'Soy el subtitulo de mi componente',
+            nombre: ''
         }
     }
 }
